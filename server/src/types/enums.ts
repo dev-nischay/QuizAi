77fd08cef5ad888f0f07e52050a8ac98de976bf6
dev-nischay <nischay.mehn@gmail.com>
2026-01-02 23:56:27 +0530

    seprated shared types

--- server/src/types/enums.ts
+++
@@ -1,7 +0,0 @@
-export enum httpStatus {
-  Ok = 200,
-  NotFound = 404,
-  Unauthorized = 401,
-  BadRequest = 400,
-  InternalServerError = 500,
-}
---
+++ server/src/types/global.types.ts
@@ -0,0 +1,11 @@
+import type { JwtPayload } from "jsonwebtoken";
+
+export interface Payload extends JwtPayload {
+  id: string;
+}
+
+export type Questions = {
+  text: string;
+  options: [string, string, string, string];
+  correctOptionIndex: 0 | 1 | 2 | 3;
+};
