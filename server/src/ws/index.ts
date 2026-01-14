f87b30b190493e881492f8e8f5e8abe313a322b6
dev-nischay <nischay.mehn@gmail.com>
2026-01-14 22:12:02 +0530

    cleaned index file with handlers

--- server/src/ws/index.ts
+++ server/src/ws/index.ts
@@ -1,57 +1,34 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, type RawData } from "ws";
 import type { Server } from "http";
-import authenticateWs from "./authenticate.js";
 import type { AuthWebSocket } from "./ws.types.js";
-import playerRouter from "./router/guest-router.js";
-import hostRouter from "./router/host-router.js";
 import { QuizMemory } from "./quiz.memory.js";
 import type { ClientResponse } from "./quiz/quiz.types.js";
-import { isOpen } from "./utils/isOpen.js";
-import { statusLogger } from "./utils/statusLogger.js";
+import { handleError } from "./handlers/errorHandler.js";
+import { handleUser } from "./handlers/userHandler.js";
+import { handleMessage } from "./handlers/messageHandler.js";
+import { handleClose } from "./handlers/closeHandler.js";
 export const initWebSocket = (server: Server) => {
   const wss = new WebSocketServer({ server });
-  // assuming room is already created
   wss.on("connection", (socket: AuthWebSocket, req) => {
     try {
-      const user = authenticateWs(req);
-      socket.user = user;
-      statusLogger(socket);
+      handleUser(socket, req);
     } catch (error) {
-      if (error instanceof Error) {
-        socket.close(1008, error.message);
-      }
-      console.log(error);
-      return;
+      handleError(socket, error);
     }
 
-    socket.on("message", (data: ClientResponse) => {
-      statusLogger(socket);
-
-      if (!isOpen(socket)) {
-        throw new Error("Socket Connection Failed");
-      }
-
+    socket.on("message", (rawData: RawData) => {
       try {
-        const message: ClientResponse = JSON.parse(String(data));
-
-        if (socket.user.role === "guest") {
-          return playerRouter(socket, message);
-        } else return hostRouter(socket, message);
+        handleMessage(socket, rawData);
       } catch (error) {
-        console.log(`Message Handler Error ${error}`);
-        socket.send(JSON.stringify({ error: "Message cannot be sent" }));
+        handleError(socket, error);
       }
     });
 
     socket.on("close", () => {
-      const { quizId, userId } = socket.user;
-
-      const quiz = QuizMemory.get(quizId);
-      if (quiz) {
-        const user = quiz?.users.get(userId);
-        if (user) {
-          user && delete user?.ws;
-        }
+      try {
+        handleClose(socket);
+      } catch (error) {
+        handleError(socket, error);
       }
     });
   });
