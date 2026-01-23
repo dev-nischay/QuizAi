11ae34a28889f300023a8911afad3e17ac71049d
dev-nischay <nischay.mehn@gmail.com>
2026-01-23 19:36:24 +0530

    updated router with new routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainLayout from "./layout/mainLayout";
 import AuthPage from "./components/auth/AuthPage";
+import QuizHome from "./components/quiz/quiz-home/QuizHome";
+import QuizBuilder from "./components/quiz/quiz-build/QuestionBuilder";
 
 export default function App() {
   return (
@@ -8,6 +10,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<AuthPage />} />
+          <Route path="home" element={<QuizHome />} />
+          <Route path="build" element={<QuizBuilder />} />
         </Route>
       </Routes>
     </BrowserRouter>
