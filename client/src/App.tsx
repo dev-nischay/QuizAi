89069e40c4b07da8ad24091a440d53d8856e8549
dev-nischay <nischay.mehn@gmail.com>
2026-01-26 03:46:39 +0530

    updated routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainLayout from "./layout/mainLayout";
 import AuthPage from "./components/auth/AuthPage";
 import QuizHome from "./components/quiz/quiz-home/QuizHome";
-import QuizBuilder from "./components/quiz/quiz-build/QuestionBuilder";
+import QuizBuilderPage from "./components/quiz/quiz-build/QuizBuilderPage";
 
 export default function App() {
   return (
@@ -11,7 +11,7 @@ export default function App() {
         <Route path="/" element={<MainLayout />}>
           <Route index element={<AuthPage />} />
           <Route path="home" element={<QuizHome />} />
-          <Route path="build" element={<QuizBuilder />} />
+          <Route path="build" element={<QuizBuilderPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
