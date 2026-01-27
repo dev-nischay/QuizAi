b9d704a26c971d268c9774639f83d8b24bf1abc6
dev-nischay <nischay.mehn@gmail.com>
2026-01-27 19:53:57 +0530

    updated mainlayout for mobile

--- client/src/layout/mainLayout.tsx
+++ client/src/layout/mainLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function MainLayout() {
   return (
-    <div className="min-h-screen  relative bg-black text-white  px-5 py-2 overflow-hidden">
+    <div className="min-h-screen  relative bg-black text-white px-2 lg:px-5 py-2 overflow-hidden">
       {/* Animated background */}
 
       <div className="absolute inset-0">
