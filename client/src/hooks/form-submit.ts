48757352d5cb6b5e9d7f82bf88089b2ca384a2c4
dev-nischay <nischay.mehn@gmail.com>
2026-02-15 00:27:58 +0530

    added form submit hook with validation

---
+++ client/src/hooks/form-submit.ts
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import type { ZodType } from "zod";
+
+export const useFormSubmit = <T>() => {
+  const [fieldErrors, setFieldErrors] = useState<T>();
+  const [submitCount, setSubmitCount] = useState(0);
+  const validator = (data: T, schema: ZodType) => {
+    const result = schema.safeParse(data);
+    if (!result.success) {
+      // handle error
+      console.log(result.error.issues);
+      let readableError = Object.fromEntries(
+        result.error.issues.map((issue) => [issue.path.join(), issue.message]),
+      ) as T;
+      setFieldErrors(readableError);
+      setSubmitCount((prev) => prev + 1);
+      return false;
+    }
+
+    return true;
+    // fetch
+  };
+
+  return {
+    validator,
+    fieldErrors,
+    submitCount,
+    setFieldErrors,
+  };
+};
