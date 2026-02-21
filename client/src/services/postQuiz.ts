98fbd157c45dc2f4295e83cb8a6b8b99605c5834
dev-nischay <nischay.mehn@gmail.com>
2026-02-22 02:40:03 +0530

    fixed bug in service

--- client/src/services/postQuiz.ts
+++ client/src/services/postQuiz.ts
@@ -1,7 +1,6 @@
 import type { QuizFormData } from "../components/quiz/quiz.types";
 import { api } from "./api";
-import { QuizFormData } from "../components/quiz/quiz.types";
 export const submitQuiz = async (data: QuizFormData) => {
-  const res = await api.post("/api/quiz/");
+  const res = await api.post("/api/quiz/", data);
   return res.data;
 };
