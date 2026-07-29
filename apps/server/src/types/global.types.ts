f28a67af16b7fe49b0dc9ac958e021040eb22ac7
dev-nischay <nischay.mehn@gmail.com>
2026-07-29 23:55:16 +0530

    removed types (exits in global pacakge)

--- apps/server/src/types/global.types.ts
+++ apps/server/src/types/global.types.ts
@@ -4,12 +4,3 @@ export interface Payload extends JwtPayload {
   userId: string;
   username: string;
 }
-
-export type Question = {
-  _id: string;
-  text: string;
-  options: [string, string, string, string];
-  correctOptionIndex: Options;
-};
-
-export type Options = 0 | 1 | 2 | 3;
